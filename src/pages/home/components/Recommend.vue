606d823ab4e88cce35550faaf2a8b6ab645cf7f5
huaihao <huaihao@citcc.cn>
2018-12-27 21:15:52 +0800

    add recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+          <img class="item-img" :src="item.imgUrl" />
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+	export default {
+		name: 'HomeRecommend',
+    data () {
+      return {
+        recommendList: [{
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1712/95/95f38f28a6ff19cba3.img.jpg_200x200_af967e35.jpg',
+          title: '水奥雪世界',
+          desc: '水奥雪世界，你的世界水奥雪世界，你的世界水奥雪世界，你的世界水奥雪世界，你的世界水奥雪世界，你的世界水奥雪世界，你的世界'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1712/95/95f38f28a6ff19cba3.img.jpg_200x200_af967e35.jpg',
+          title: '水奥雪世界',
+          desc: '水奥雪世界，你的世界水奥雪世界，你的世界水奥雪世界，你的世界水奥雪世界，你的世界水奥雪世界，你的世界水奥雪世界，你的世界'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1712/95/95f38f28a6ff19cba3.img.jpg_200x200_af967e35.jpg',
+          title: '水奥雪世界',
+          desc: '水奥雪世界，你的世界水奥雪世界，你的世界水奥雪世界，你的世界水奥雪世界，你的世界水奥雪世界，你的世界水奥雪世界，你的世界'
+        }]
+      }
+    }
+	}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-index: .2rem
+  .item
+    overflow: hidden
+    display: flex
+    height: 1.9rem
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color: #eee
+        ellipsis()
+      .item-button
+        line-height: .44rem
+        margin-top: .16rem
+        background: #ff9300
+        padding: 0 .2rem
+        border-radius: .06rem
+        color: #fff
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl" />
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+	export default {
+		name: 'HomeRecommend',
+    data () {
+      return {
+        recommendList: [{
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg',
+          title: '水奥雪世界',
+          desc: '水奥雪世界，你的世界水奥雪世界，你的世界水奥雪世界，你的世界水奥雪世界，你的世界水奥雪世界，你的世界水奥雪世界，你的世界'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg',
+          title: '水奥雪世界',
+          desc: '水奥雪世界，你的世界水奥雪世界，你的世界水奥雪世界，你的世界水奥雪世界，你的世界水奥雪世界，你的世界水奥雪世界，你的世界'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg',
+          title: '水奥雪世界',
+          desc: '水奥雪世界，你的世界水奥雪世界，你的世界水奥雪世界，你的世界水奥雪世界，你的世界水奥雪世界，你的世界水奥雪世界，你的世界'
+        }]
+      }
+    }
+	}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-index: .2rem
+  .item-img-wrapper
+    overflow: hidden
+    height: 0
+    padding-bottom: 33.9%
+    .item-img
+      width: 100%
+  .item-info
+    padding: .1rem
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: .4rem
+      color: #eee
+      ellipsis()
+</style>
