4a9389dd3124d3d625fbdac2130f41cdfcb6f6c8
huaihao <huaihao@citcc.cn>
2019-01-02 18:30:22 +0800

    ajax city

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,18 +1,15 @@
 <template>
   <ul class="list">
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
+    <li class="item" v-for="(item, key) of cities" :key="key">{{key}}</li>
   </ul>
 </template>
 
 <script>
 	export default {
-		name: 'CityAlphabet'
+		name: 'CityAlphabet',
+    props: {
+      cities: Object
+    }
 	}
 </script>
 
@@ -23,7 +20,7 @@
     flex-direction: column
     justify-content: center
     position: absolute
-    top: 1.58rem
+    top: 3rem
     right: 0
     bottom: 0
     width: .4rem
